e61c2a9b84681274dce6ee7743c3fadf5da2cca2
Marco Carujo <mamcarujo@gmail.com>
2019-05-26 22:06:00 -0300

    model neogiacoes com get total

--- client/js/models/ListaNegociacoes.js
+++ client/js/models/ListaNegociacoes.js
@@ -1,11 +1,22 @@
 class ListaNegociacoes {
   constructor() {
-    this.negociacoes = Array;
+    this.negociacoes = [];
   }
-  adiciona(negociacoes) {
-    this.negociacoes.push(negociacoes);
+  adiciona(negociacao) {
+    this.negociacoes.push(negociacao);
   }
   getNegociacoes() {
     return this.negociacoes;
+  }
+  getTotal() {
+    if (this.negociacoes.length == 0) {
+      return 0;
+    } else {
+      let total = 0;
+      this.negociacoes.map(item => {
+        total += item.volume;
+      });
+      return total;
+    }
   }
 }
